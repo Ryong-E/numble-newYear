dfbdcc674d70c307e11d85cb5f528e7486c7e850
Ryong-E <think8867@gmail.com>
2023-01-12 14:50:16 +0900

    fix: webpack babel,css-loader추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,32 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
 module.exports = {
-  context: __dirname + '/src',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
-    path: __dirname + '/src/public',
+    path: path.resolve(__dirname, 'src', 'public'),
     filename: 'main.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|pages)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: 'css-loader',
+            options: { import: true },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
