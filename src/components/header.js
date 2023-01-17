9222f19621f6b4c7c40427f440b9b379620dc6d7
Ryong-E <think8867@gmail.com>
2023-01-17 22:13:23 +0900

    style: 로고 토끼 추가

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ const Header = () => {
   return `
     <header ${isHome === '/' && `style='justify-content:flex-end;'`}>
     ${backbutton}
-        <a href='/' id='logo'>HPNY 2023</a>
+        <a href='/' id='logo'>🐇HPNY 2023</a>
     </header>
     `;
 };
