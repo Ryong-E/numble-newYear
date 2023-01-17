2ae927db8b3e1c2854c87e3856e97f70c186b52d
Ryong-E <think8867@gmail.com>
2023-01-17 22:20:39 +0900

    feat(disableImageUpload): 이미지 업로드시 해당 버튼 상태변경 구현

---
+++ src/utils/disableImageUpload.js
@@ -0,0 +1,9 @@
+function disableImageUpload() {
+  const imageBox = document.querySelector('#image-upload-box');
+  imageBox.innerHTML = '<h1>이미지 추가 완료</h1>';
+  imageBox.style.backgroundColor = 'grey';
+  imageBox.style.cursor = 'not-allowed';
+  imageBox.style.backgroundImage = 'none';
+}
+
+export default disableImageUpload;
