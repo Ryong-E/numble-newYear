93823a12198859f963467cb7ec272d9dbdcc97df
Ryong-E <think8867@gmail.com>
2023-01-19 15:40:23 +0900

    refactor: isHome boolean값을 가지도록 변경

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,14 +1,14 @@
 const Nav = () => {
   let backbutton = '';
-  const isHome = location.pathname;
+  const isHome = location.pathname === '/';
 
-  if (isHome !== '/') {
+  if (!isHome) {
     backbutton = `<a href='/'><img src="https://hpny-1.s3.ap-northeast-2.amazonaws.com/icon_chevron_left.svg" id="back-icon" class='nav-link'></img></a>`;
   }
 
   return `
-    <nav ${isHome === '/' && `style='justify-content:flex-end;'`}>
-    ${backbutton}
+    <nav ${isHome && `style='justify-content:flex-end;'`}>
+        ${backbutton}
         <a href='/' id='logo' class='nav-link'><img src='http://61.97.184.251/favicon.png'>HPNY 2023</a>
     </nav>
     `;
