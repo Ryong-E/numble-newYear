dbe533e5466b7ffad628dd40323790d1a027d130
Ryong-E <think8867@gmail.com>
2023-01-19 15:39:39 +0900

    refactor: 파라미터 e -> event로 변경

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import router from './router.js';
 window.addEventListener('popstate', router);
 
 window.addEventListener('DOMContentLoaded', () => {
-  document.body.addEventListener('click', (e) => {
-    if (e.target.className === 'nav-link') {
-      e.preventDefault();
-      history.pushState(null, null, e.target.href);
+  document.body.addEventListener('click', (event) => {
+    if (event.target.className === 'nav-link') {
+      event.preventDefault();
+      history.pushState(null, null, event.target.href);
       router();
     }
   });
