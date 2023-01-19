2d8a5b4af0fe2fb92b3c91ee7fd0f803ee34ba4a
Ryong-E <think8867@gmail.com>
2023-01-19 18:26:18 +0900

    feat: Cache-Control max-age 1년 설정

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static(path.resolve(__dirname, 'src', 'public')));
+app.use(
+  '/',
+  express.static(path.resolve(__dirname, 'src', 'public'), {
+    setHeaders: function (res, path, stat) {
+      res.setHeader('Cache-Control', 'max-age=31536000');
+    },
+  }),
+);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve('src', 'public', 'index.html'));
